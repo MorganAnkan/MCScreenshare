0a76c2a371c352d47f7ba5f314b1295c8c08ec38
MorganAnkan <iamgoodatmc6969@gmail.com>
2020-07-27 20:08:10 +0200

    oops forgot a "!"

--- src/main/java/mc/screenshare/commands/TypeCommand.java
+++ src/main/java/mc/screenshare/commands/TypeCommand.java
@@ -14,7 +14,7 @@ public class TypeCommand implements CommandExecutor {
             sender.sendMessage("u must have some args lmao");
             return true;
         }
-        if(PacketHandler.isConnected()) {
+        if(!PacketHandler.isConnected()) {
             sender.sendMessage("hmmm not connected");
         } else {
             sender.sendMessage("attempting to make the client type: "+String.join(" ", args));
